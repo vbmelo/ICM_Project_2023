a00c914bd1b0ad89b9a515cc807bc711df6429d2
Marcel de Araújo Santos Souza <marcelaraujosantossouza19@gmail.com>
2023-04-15 21:06:38 +0100

    Diapason ""Working""

---
+++ SongLearn/app/src/main/java/com/example/songlearn/DiapasonActivity.kt
@@ -0,0 +1,34 @@
+package com.example.songlearn
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.songlearn.R
+
+class DiapasonActivity : AppCompatActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diapason)
+        val diapason_button : Button = findViewById(R.id.diapason_button)
+        val diapason_stop_button : Button = findViewById(R.id.stopDiapason)
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.a4)
+        diapason_button.setOnClickListener {
+            mediaPlayer.start()
+        }
+
+        diapason_stop_button.setOnClickListener{
+            mediaPlayer.stop()
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+    }
+}
